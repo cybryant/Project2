e61375103dd82e29675344c583549683795c0cb3
Cherie <50756236+cybryant@users.noreply.github.com>
2022-10-31 01:31:25 -0400

    added additional widgets & notes layer
    added location, editor and basemap toggle widgets; plus some comment housekeeping

--- KlappPhipps.js
+++ KlappPhipps.js
@@ -1,22 +1,34 @@
 require([
+  "esri/config",
   "esri/Map",
   "esri/views/MapView",
   "esri/layers/FeatureLayer",
   "esri/views/layers/LayerView",
   "esri/widgets/Home",
   "esri/widgets/Zoom",
+  "esri/widgets/BasemapToggle",
   "esri/widgets/Editor",
+  "esri/widgets/Locate",
   "esri/layers/support/FeatureEffect"
 ], (
+  esriConfig,
   Map,
   MapView,
   FeatureLayer,
   LayerView,
   Home,
   Zoom,
+  BasemapToggle,
   Editor,
+  Locate,
   FeatureEffect
 ) => {
+  //*********************************
+  // API KEY NEEDED FOR LOCATE BUTTON FUNCTIONALITY
+  //*********************************
+  esriConfig.apiKey =
+    "AAPK6027c4c6c95b4b519c299cb7241fd1cacQ9_liBgjmYxcyyIkrLe0WjpWf9oGPt-VuUX05UqCy82EKlLp5v1nOibD7YOE__U";
+
   //*********************************
   // LAYER VARIABLES & SYMBOLOGY RENDERERS
   //*********************************
@@ -29,7 +41,7 @@ require([
     definitionExpression: "PARKNAME = 'Elinor Klapp-Phipps Park'"
   });
 
-  // define renderer for boundary symbology
+  // renderer for park polygon layer
   const boundRenderer = {
     type: "simple",
     symbol: {
@@ -49,6 +61,14 @@ require([
     renderer: boundRenderer
   });
 
+  // notes layer (user editable)
+  const notes = new FeatureLayer({
+    portalItem: {
+      id: "e1a2bc5263e64bbba407e356f818e55a"
+    },
+    title: "Notes"
+  });
+
   //*********************************
   // REQUIRED MAP OBJECTS
   //*********************************
@@ -60,7 +80,7 @@ require([
     //       id: "4f2e99ba65e34bb8af49733d9778fb8e",
     //     },
     basemap: "gray-vector",
-    layers: [boundary, trails]
+    layers: [trails, boundary, notes]
   });
 
   // mapView
@@ -76,31 +96,61 @@ require([
   });
 
   //*********************************
-  // ????
+  // CONFIGURE EDITOR WIDGET
   //*********************************
+  // variable to hold configuration parameters
+  let notesEditConfig;
+  /* access layer using forEach(); Editor layerInfo docs just show all this under the new Editor properties with out the forEach(), but that resulted in an empty editor element; this method is from one of the samples for multiple configurations within one widget)*/
+  view.map.layers.forEach((notes) => {
+    if (notes.title === "Notes") {
+      notesEditConfig = {
+        layer: notes,
+        formTemplate: {
+          elements: [{ type: "field", fieldName: "Notes", label: "Notes" }]
+        }
+      };
+    }
+  });
 
   //*********************************
-  // CREATE WIDGET VARIABLES
+  // CREATE WIDGETS
   //*********************************
-  // const titleDiv = document.getElementById("titleDiv");
-  // const editor = new Editor({ view: view });
   const home = new Home({ view: view });
-  const zoom = new Zoom({ view: view });
+  //const zoom = new Zoom({ view: view });
+  const basemapToggle = new BasemapToggle({
+    view: view,
+    nextBasemap: "streets-vector"
+  });
+  const editor = new Editor({
+    view: view,
+    layerInfos: [{ layer: notesEditConfig }],
+    // TO DO - this isn't eliminating the snapping options
+    snappingOptions: { enabled: false }
+    // widgetVisible: false
+  });
+  const locate = new Locate({
+    view: view,
+    // set to false so it doesn't change rotation of the map
+    useHeadingEnabled: false,
+    // set custom zoom functionality
+    goToOverride: function (view, options) {
+      options.target.scale = 1500;
+      return view.goTo(options.target);
+    }
+  });
 
   //*********************************
-  // DEFINE EDITOR FUNCTIONALITY
-  //*********************************
-
-  //*********************************
-  // ADD WIDGETS TO USER INTERFACE
+  // ADD ALL WIDGETS TO USER INTERFACE
   //*********************************
   view.ui.empty("top-left");
-  // view.ui.add(editor, "top-left");
   view.ui.add(home, "top-right");
-  view.ui.add(zoom, "top-right");
+  //view.ui.add(zoom, "top-right");
+  view.ui.add(basemapToggle, "top-right");
+  view.ui.add(editor, "top-left");
+  view.ui.add(locate, "bottom-left");
 
   //*********************************
-  // CREATE LAYERVIEW OBJECT TO FILTER
+  // CREATE 'trails' LAYERVIEW OBJECT TO FILTER BY TRAIL TYPE
   //*********************************
   /* 'layerView' versus 'layer' allows filtering on the client side rather than the server side, so no callback is needed & performance is faster */
 
@@ -114,9 +164,8 @@ require([
   });
 
   //*********************************
-  // FILTERS FOR TRAIL TYPES
+  // VARIABLES FOR EACH TRAIL TYPE FILTER
   //*********************************
-  // variables for each trail type filter
   const sharedFilter = {
     where: "CATEGORY='Shared-Use Equestrian'"
   };
@@ -130,6 +179,8 @@ require([
   //*********************************
   // EVENT LISTENER FOR RADIO BUTTONS
   //*********************************
+  //TO DO - set radio buttons initial state to unchecked
+  // Chrome sets them that way, but some browsers (i.e., Firefox) appear to autocheck the first one
   document.getElementById("filterDiv").addEventListener("change", (event) => {
     let target = event.target;
     switch (target.id) {
